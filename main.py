d6f1e45a0f744f72f964c0fe82d05cb02c46a0f3
Antoine Perrin <a.perrindelort@laposte.net>
2024-03-21 18:01:45 +0100

    Added main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 14:01:54 2024
+
+@author: Antoine
+"""
+import runpy
+import warnings
+
+from util.arg_parser import ProjectEulerArgumentParser
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore", category=RuntimeWarning)
+    
+    parser = ProjectEulerArgumentParser()
+    args = parser.parse_args()
+
+    # Get the module name from the command line argument
+    script_name = f"scripts.problem_{args.problem_number}"
+
+    # Get the module object using __import__
+    script = __import__(script_name)
+
+    # Run the __main__ function of the module using runpy
+    runpy.run_module(script_name, run_name="__main__", alter_sys=True)
