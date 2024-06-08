79e86e5600cd9028f3a7580b63da8d57a4a2b440
Antoine Perrin--Delort <103105422+perrindelort@users.noreply.github.com>
2024-06-08 03:16:14 +0200

    Renaming 'arg-parser.py' and resolving imports
    arg-parser.py -> parser.py
    Imports resolutions

--- util/__init__.py
+++ util/__init__.py
@@ -5,5 +5,5 @@ This package includes miscellaneous collection of variables and functions
 from util.statements import get_statement
 from util.titles import get_title
 from util.util import format_statement_answer, timing, load_data_from_txt
-from util.arg_parser import ProjectEulerArgumentParser
+from util.parser import ProjectEulerArgumentParser
 from util.color_checker import display_colors
